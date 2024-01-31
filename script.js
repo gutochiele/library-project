e8f2f34cc759fadbb3b408d46aa48c8f17cec631
gutochiele <63303276+gutochiele@users.noreply.github.com>
2024-01-31 19:53:10 +0100

    worked out basic script for adding books

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-const myLibrary = [];
-
-function Book() {
-  // the constructor...
+function Book(title, author, pages, read, index) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.index = index;
 }
 
-function addBookToLibrary() {
-  // do stuff here
+const myLibrary = [];
+
+
+document.getElementById("add-book").addEventListener("submit", function(event) {
+    event.preventDefault();
+    addBook(event);
+});
+
+function addBook(){
+    console.log("addBook function is called");
+    const title = document.getElementById("book-title").value;
+    const author = document.getElementById("author").value;
+    const pages = document.getElementById("pages").value;
+    const read = document.getElementById("read").checked;
+    const index = myLibrary.length;
+
+    let addToLibrary = new Book(title, author, pages, read, index);
+    myLibrary.push(addToLibrary)
 }
